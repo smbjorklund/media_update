bffd35d21c449850f3fd10137a82e135601085c8
Gisle Aas <gisle.aas@it.uib.no>
2012-02-10 14:27:17 +0100

    Support PI- and BS- links

--- doc/htmlify.py
+++ doc/htmlify.py
@@ -5,6 +5,8 @@ import os
 
 def md_expand(text, filename):
     text = re.sub(r'(\bJEW-\d+\b)', r'[\1](https://jira.uib.no/browse/\1)', text)
+    text = re.sub(r'(\bPI-(\d+)\b)', r'[\1](http://prosjekt.uib.no/issues/\2)', text)
+    text = re.sub(r'(\bBS-(\d+)\b)', r'[\1](https://bs.uib.no/?module=issues&action=view&tid=\2)', text)
     text = re.sub(r'\bapidoc:(\w+(?:\.\w+)*(?:/[\w.-]+)?)', r'http://apidoc.app.uib.no/system/\1.html', text)
 
     def expand_date(m):
