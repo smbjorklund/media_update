03fded43f709618ca667dcd2504fd0a80cff0177
Gisle Aas <gisle.aas@it.uib.no>
2012-05-15 14:31:06 +0200

    A selenium test

---
+++ test-se/test_site.py
@@ -0,0 +1,20 @@
+import unittest
+from selenium import webdriver
+
+from os import readlink
+from os.path import basename
+
+class SiteTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+        self.site_url = 'http://' + basename(readlink('../site'))
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_homepage(self):
+        self.driver.get(self.site_url)
+        self.assertEqual(self.driver.title, "Universitetet i Bergen")
+        self.driver.save_screenshot('home.png')
