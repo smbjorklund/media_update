f9a651e23f0ae18ce815f03eaa639ccbb38216ce
Gisle Aas <gisle.aas@it.uib.no>
2012-06-22 08:57:50 +0200

    Expand RTS issue links

--- doc/htmlify.py
+++ doc/htmlify.py
@@ -7,6 +7,7 @@ def md_expand(text, filename):
     text = re.sub(r'(\bJEW-\d+\b)', r'[\1](https://jira.uib.no/browse/\1)', text)
     text = re.sub(r'(\bPI-(\d+)\b)', r'[\1](http://prosjekt.uib.no/issues/\2)', text)
     text = re.sub(r'(\bBS-(\d+)\b)', r'[\1](https://bs.uib.no/?module=issues&action=view&tid=\2)', text)
+    text = re.sub(r'(\bRTS-(\d+)\b)', r'[\1](https://rts.uib.no/issues/\2)', text)
     text = re.sub(r'\bapidoc:(\w+(?:\.\w+)*(?:/[\w.-]+)?)', r'http://apidoc.app.uib.no/system/\1.html', text)
 
     def expand_date(m):
