868bc2544af4d54e2ca87475c318e5b488950cd0
Stein Magne Bjorklund <stein.bjorklund@adm.uib.no>
2012-09-12 14:33:04 +0200

    Make sure that grid in on by default for logged in users.

--- themes/uib_zen/template.php
+++ themes/uib_zen/template.php
@@ -133,7 +133,7 @@ function uib_zen_preprocess_html(&$variables, $hook) {
   if (!$variables['user']->uid == '0') {
     $user = user_load($variables['user']->uid);
 
-    if (!empty($user->field_grid['und'][0]['value']) == '1')
+    if (empty($user->field_grid['und']) || !empty($user->field_grid['und'][0]['value']) == '0')
       $variables['classes_array'][] = 'grid';
   }
 }
