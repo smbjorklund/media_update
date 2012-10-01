517309b3c23f4585a6a3fbf2c7a222291e5d37ad
Trude Leknes <trude.leknes@adm.uib.no>
2012-10-01 12:27:07 +0200

    added responsive prototype in the proto folder

---
+++ proto/responsive/js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($){
+  $(".menu-button").toggle(function(e){
+      $(".sf-menu").animate({top:'0'});
+    }, function(e){
+      $(".sf-menu").animate({top:'-210px'});
+    });
+});
