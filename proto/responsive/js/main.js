607a94652193057f245ead702a9b1714673f34cb
Trude Leknes <trude.leknes@adm.uib.no>
2012-10-02 14:34:51 +0200

    RTS-477 Optimized the global menu for the iphone

--- proto/responsive/js/main.js
+++ proto/responsive/js/main.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function($){
   $(".menu-button").toggle(function(e){
       $(".sf-menu").animate({top:'0'});
     }, function(e){
-      $(".sf-menu").animate({top:'-210px'});
+      $(".sf-menu").animate({top:'-340px'});
     });
 });
