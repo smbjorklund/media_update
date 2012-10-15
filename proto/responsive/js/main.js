f2f23910a4387e9f25bbbaf0aa15d39b8df9f757
Trude Leknes <trude.leknes@adm.uib.no>
2012-10-15 15:48:37 +0200

    RTS-605 Added prototype for ansatt and nyhet

--- proto/responsive/js/main.js
+++ proto/responsive/js/main.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function($){
   $(".menu-button").toggle(function(e){
-      $(".sf-menu").animate({top:'0'});
+      $(".sf-menu").animate({top:'30px'});
     }, function(e){
-      $(".sf-menu").animate({top:'-340px'});
+      $(".sf-menu").animate({top:'-500'});
     });
+
 });
