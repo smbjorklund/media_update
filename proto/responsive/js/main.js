2f509ba9523687569b0395a935231ead75811415
Trude Leknes <trude.leknes@adm.uib.no>
2012-11-02 11:27:43 +0100

    RTS-605 Ferdigstilt ansattmenyen og småplukk ellers

--- proto/responsive/js/main.js
+++ proto/responsive/js/main.js
@@ -6,3 +6,11 @@ jQuery(document).ready(function($){
     });
 
 });
+jQuery(document).ready(function($){
+  $(".menu-button").toggle(function(e){
+      $(".sf-menu-2").animate({top:'100px'});
+    }, function(e){
+      $(".sf-menu-2").animate({top:'-800'});
+    });
+
+});
