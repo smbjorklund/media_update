cba39492dd7148489db8e7dfe9e30d1a18b03769
Are Johannessen <are.johannessen@it.uib.no>
2013-01-31 11:32:50 +0100

    RTS-1296 Testing variables to prevent errors

--- themes/uib_zen/template.php
+++ themes/uib_zen/template.php
@@ -189,10 +189,14 @@ function uib_zen_preprocess_page(&$variables, $hook) {
     }
   }
 
-  if ($variables['node']->type == 'area') {
-    $nid = $variables['node']->nid;
-    $variables['page']['bottom']['field_uib_logo'] = $variables['page']['content']['system_main']['content']['nodes'][$nid]['field_uib_logo'];
-    unset($variables['page']['content']['system_main']['content']['nodes'][$nid]['field_uib_logo']);
+  if (isset($variables['node'])) {
+    if ($variables['node']->type == 'area') {
+      $nid = $variables['node']->nid;
+      if (isset($variables['page']['content']['system_main']['content']['nodes'][$nid]['field_uib_logo'])) {
+        $variables['page']['bottom']['field_uib_logo'] = $variables['page']['content']['system_main']['content']['nodes'][$nid]['field_uib_logo'];
+        unset($variables['page']['content']['system_main']['content']['nodes'][$nid]['field_uib_logo']);
+      }
+    }
   }
 }
 // */
