smbjorklund/media_update

commit 35527f64abaf6fb0ce9a615028a9ae5fd71135f6

Fixing code formatting warnings discovered in coder-review

themes/uib_zen/templates/views-view-table--uib-webdesk--page.tpl.php
@@ -26,7 +26,7 @@ if (in_array('level 1', $user->roles) || in_array('level 2', $user->roles))
   $user_role = FALSE;
 ?>
 
-<table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
+<table <?php if ($classes) ? print 'class="'. $classes . '" ' : ''; } ?><?php print $attributes; ?>>
   <?php if (!empty($title)) : ?>
     <caption><?php print $title; ?></caption>
   <?php endif; ?>
@@ -36,12 +36,12 @@ if (in_array('level 1', $user->roles) || in_array('level 2', $user->roles))
         <?php foreach ($header as $field => $label): ?>
 
           <?php if (!$user_role): ?>
-            <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+            <th <?php if ($header_classes[$field]) ? print 'class="'. $header_classes[$field] . '" ' : ''; ?>>
               <?php print $label; ?>
             </th>
           <?php else: ?>
             <?php if ($header_classes[$field] != 'views-field views-field-views-bulk-operations'): ?>
-              <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+              <th <?php if ($header_classes[$field]) ? print 'class="'. $header_classes[$field] . '" ' : ''; ?>>
                 <?php print $label; ?>
               </th>
             <?php endif; ?>
@@ -52,15 +52,15 @@ if (in_array('level 1', $user->roles) || in_array('level 2', $user->roles))
   <?php endif; ?>
   <tbody>
     <?php foreach ($rows as $row_count => $row): ?>
-      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+      <tr <?php if ($row_classes[$row_count]) ? print 'class="' . implode(' ', $row_classes[$row_count]) .'"' : ''; ?>>
         <?php foreach ($row as $field => $content): ?>
           <?php if (!$user_role): ?>
-            <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+            <td <?php if ($field_classes[$field][$row_count]) ? print 'class="'. $field_classes[$field][$row_count] . '" ' : ''; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
               <?php print $content; ?>
             </td>
           <?php else: ?>
             <?php if ($field_classes[$field][$row_count] != 'views-field views-field-views-bulk-operations'): ?>
-              <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+              <td <?php if ($field_classes[$field][$row_count]) ? print 'class="'. $field_classes[$field][$row_count] . '" ' : ''; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
                 <?php print $content; ?>
               </td>
             <?php endif; ?>

themes/uib_zen/templates/views-view-unformatted--ansatte--page.tpl.php
@@ -12,7 +12,7 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div <?php if ($classes_array[$id]) { print 'class="' . $classes_array[$id] .'"';  } ?>>
+  <div <?php if ($classes_array[$id]) ? print 'class="' . $classes_array[$id] .'"' : ''; ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>

themes/uib_zen/templates/views-view-unformatted--newsarchive.tpl.php
@@ -12,7 +12,7 @@
     <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div <?php if ($classes_array[$id]) { print 'class="' . $classes_array[$id] .'"';  } ?>>
+  <div <?php if ($classes_array[$id]) ? print 'class="' . $classes_array[$id] .'"' : ''; ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>

themes/uib_zen/template.php
@@ -155,7 +155,7 @@ function uib_zen_preprocess_page(&$variables, $hook) {
   *    Full url to area home. Defaults to front page if no relevant area present
   *    Corresponding modifications to use page_title and page_title_link are found in page.tpl.php
   */
-  if (arg(0) == 'node' && arg(1) != 'add'){
+  if (arg(0) == 'node' && arg(1) != 'add') {
     if ($variables['language']->language == 'nb') {
       $variables['global_menu'] = menu_navigation_links('menu-global-menu-no');
     }
@@ -238,10 +238,10 @@ function uib_zen_preprocess_node(&$variables, $hook) {
       if ($variables['field_uib_area_type']['und']['0']['value'] == 'unit') {
          $variables ['classes_array'][] = t('unit_node');
       }
-      if(isset($variables['content']['field_uib_profiled_article'])) {
+      if (isset($variables['content']['field_uib_profiled_article'])) {
         $weight = $variables['content']['field_uib_profiled_article']['#weight'];
         unset($variables['content']['field_uib_profiled_article']);
-        $variables['content']['field_uib_profiled_article'][]['#markup'] = views_embed_view('area_slideshow','default', $variables['nid']);
+        $variables['content']['field_uib_profiled_article'][]['#markup'] = views_embed_view('area_slideshow', 'default', $variables['nid']);
         $variables['content']['field_uib_profiled_article']['#weight'] = $weight;
       }
       $tmp_block_html = views_embed_view('faculty_departments_kids', 'block', $variables['nid']);
@@ -306,7 +306,8 @@ function uib_zen_preprocess_node(&$variables, $hook) {
           }
           hide($variables['content']['group_article_main']['field_uib_byline']);
           $variables['content']['group_article_main']['uib_news_byline'] = array('#markup' => "<div class=\"uib-news-byline\">" . $uib_news_byline . "</div>");
-        } else {
+        }
+        else {
           if (isset ($variables['node']->name)) {
             $uib_news_byline = t('By') . ' ' . $variables['node']->name;
             $variables['content']['group_article_main']['uib_news_byline'] = array('#markup' => '<div class="uib-news-byline">' . $uib_news_byline . "</div>");
@@ -341,14 +342,14 @@ function uib_zen_preprocess_node(&$variables, $hook) {
       }
 
       // Do not show related area on employee pages [RTS 1225]
-      if (stripos($variables['node_url'],'foransatte') !== FALSE OR stripos($variables['node_url'],'foremployees') !== FALSE) {
+      if (stripos($variables['node_url'], 'foransatte') !== FALSE OR stripos($variables['node_url'], 'foremployees') !== FALSE) {
          hide($variables['content']['group_article_sidebar']['field_uib_area']);
       }
     }
 
     if (in_array($variables['title'], array('Ansattsider', 'Employee Pages'))) {
-      drupal_add_js(drupal_get_path('theme', 'uib_zen').'/js/hide_links.js',
-        array('group' => JS_THEME,)
+      drupal_add_js(drupal_get_path('theme', 'uib_zen') . '/js/hide_links.js',
+        array('group' => JS_THEME, )
       );
     }
   }
@@ -377,7 +378,7 @@ function uib_zen_menu_link(array $variables) {
   }
 
   if (isset($variables['element']['#bid']) && ($variables['element']['#bid']['delta'] == 'top-area-menu') && ($element['#original_link']['depth'] == 2))
-    $output = '<a href="#">'.$element["#title"].'</a> ';
+    $output = '<a href="#">' . $element["#title"] . '</a> ';
   else
     $output = l($element['#title'], $element['#href'], $element['#localized_options']);
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
@@ -483,7 +484,7 @@ function uib_zen_preprocess_block(&$variables, $hook) {
 * @param $hook
 *   The name of the template being rendered
 */
-function uib_zen_preprocess_field (&$variables, $hook) {
+function uib_zen_preprocess_field(&$variables, $hook) {
   //arrays containg field_name_css of blocks where the title is getting colored squares
   $blue_block = array(
     'field-uib-area',

commit 6bfb71d1c82fa2ef466c9d78611f376612f85e7d

Fixed an error in syntax introduced in 35527f6

themes/uib_zen/templates/views-view-table--uib-webdesk--page.tpl.php
@@ -26,7 +26,7 @@ if (in_array('level 1', $user->roles) || in_array('level 2', $user->roles))
   $user_role = FALSE;
 ?>
 
-<table <?php if ($classes) ? print 'class="'. $classes . '" ' : ''; } ?><?php print $attributes; ?>>
+<table <?php print ($classes) ? 'class="'. $classes . '" ' : ''; ?><?php print $attributes; ?>>
   <?php if (!empty($title)) : ?>
     <caption><?php print $title; ?></caption>
   <?php endif; ?>
@@ -36,12 +36,12 @@ if (in_array('level 1', $user->roles) || in_array('level 2', $user->roles))
         <?php foreach ($header as $field => $label): ?>
 
           <?php if (!$user_role): ?>
-            <th <?php if ($header_classes[$field]) ? print 'class="'. $header_classes[$field] . '" ' : ''; ?>>
+            <th <?php print ($header_classes[$field]) ? 'class="' . $header_classes[$field] . '" ' : ''; ?>>
               <?php print $label; ?>
             </th>
           <?php else: ?>
             <?php if ($header_classes[$field] != 'views-field views-field-views-bulk-operations'): ?>
-              <th <?php if ($header_classes[$field]) ? print 'class="'. $header_classes[$field] . '" ' : ''; ?>>
+              <th <?php print 'class="' . ($header_classes[$field]) ? $header_classes[$field] : '' . '"'; ?>>
                 <?php print $label; ?>
               </th>
             <?php endif; ?>
@@ -52,15 +52,15 @@ if (in_array('level 1', $user->roles) || in_array('level 2', $user->roles))
   <?php endif; ?>
   <tbody>
     <?php foreach ($rows as $row_count => $row): ?>
-      <tr <?php if ($row_classes[$row_count]) ? print 'class="' . implode(' ', $row_classes[$row_count]) .'"' : ''; ?>>
+      <tr <?php print ($row_classes[$row_count]) ? 'class="' . implode(' ', $row_classes[$row_count]) . '"' : ''; ?>>
         <?php foreach ($row as $field => $content): ?>
           <?php if (!$user_role): ?>
-            <td <?php if ($field_classes[$field][$row_count]) ? print 'class="'. $field_classes[$field][$row_count] . '" ' : ''; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+            <td <?php print 'class="' . ($field_classes[$field][$row_count]) ? $field_classes[$field][$row_count] : '' . '"'; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
               <?php print $content; ?>
             </td>
           <?php else: ?>
             <?php if ($field_classes[$field][$row_count] != 'views-field views-field-views-bulk-operations'): ?>
-              <td <?php if ($field_classes[$field][$row_count]) ? print 'class="'. $field_classes[$field][$row_count] . '" ' : ''; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+              <td <?php print ($field_classes[$field][$row_count]) ? 'class="' . $field_classes[$field][$row_count] . '" ' : ''; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
                 <?php print $content; ?>
               </td>
             <?php endif; ?>

themes/uib_zen/templates/views-view-unformatted--ansatte--page.tpl.php
@@ -12,7 +12,7 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div <?php if ($classes_array[$id]) ? print 'class="' . $classes_array[$id] .'"' : ''; ?>>
+  <div <?php print ($classes_array[$id]) ? 'class="' . $classes_array[$id] . '"' : ''; ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>

themes/uib_zen/templates/views-view-unformatted--newsarchive.tpl.php
@@ -12,7 +12,7 @@
     <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div <?php if ($classes_array[$id]) ? print 'class="' . $classes_array[$id] .'"' : ''; ?>>
+  <div <?php print ($classes_array[$id]) ? 'class="' . $classes_array[$id] . '"' : ''; ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>