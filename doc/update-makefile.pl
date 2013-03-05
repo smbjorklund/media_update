47f765a9183608d62e5ced950a62368a5b6a06f2
Gisle Aas <gisle.aas@it.uib.no>
2013-03-05 10:59:23 +0100

    Make 'make makefile' update the list of HTML files

---
+++ doc/update-makefile.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+
+open(my $fh, "Makefile") || die;
+while (<$fh>) {
+    print;
+    if (/^HTML =/) {
+	for (glob('*.md')) {
+	    (my $html = $_) =~ s/\.md$/.html/;
+	    print "  $html \\\n";
+	}
+	print "\n";
+
+	# skip the old entries
+	while (<$fh>) {
+	    last if /^$/;
+	}
+    }
+}
