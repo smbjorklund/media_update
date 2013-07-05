58131c6b17c5491916f74499bc7e0bc18f8732d3
Said Berg <said.berg@uib.no>
2013-07-05 13:17:51 +0200

    RTS-3227 Renaming field for main media
    The name was causing some styling issues, so we give the articles in recent news another name

--- themes/uib_zen/templates/node--news--recent-news.tpl.php
+++ themes/uib_zen/templates/node--news--recent-news.tpl.php
@@ -93,6 +93,7 @@
     // Using l() to create link failed, the url created gave 404.
     $content['title']['#markup'] = '<h1>' . '<a href=' . $node_url . '>' . $title . '</a></h1>';
     $content['title']['#weight'] = $content['field_uib_main_media']['#weight'] + 1;
+    $content['field_uib_main_media']['#field_name'] = 'field_uib_main_media_recent_news';
 
     print render($content);
   ?>
