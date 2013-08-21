2053cd57b7a61ddb7e63c1b2a3c43003fdc3fd09
Are Johannessen <are.johannessen@it.uib.no>
2013-08-21 12:54:53 +0200

    RTS-3356 Aligned article info and the social services icons
    The article info (byline and dates) were not aligned to the social
    service icons. This commit adds the necessary classes to all elements
    and cleans up the styling to present the information in a more consistent
    way through our site.

--- themes/uib_zen/template.php
+++ themes/uib_zen/template.php
@@ -377,7 +377,7 @@ function uib_zen_preprocess_node(&$variables, $hook) {
         hide($variables['content']['group_article_main']['field_uib_byline']);
         $variables['content']['group_article_top']['uib_news_byline'] = array('#markup' => "<div class=\"uib-news-byline\">" . $uib_news_byline . "</div>");
         $variables['content']['group_article_top']['uib_news_byline']['#suffix'] = '<div class="uib-news-byline-created uib-publish-info">' . t('Created') . ' ' . format_date($variables['node']->created, 'long') . '</div><div class="uib-news-byline-last-updated uib-publish-info">' . t('Last updated') . ' ' . format_date($variables['node']->revision_timestamp, 'long') . '</div>';
-        $variables['content']['group_article_top']['#prefix'] = '<div class="article-info">';
+        $variables['content']['group_article_top']['#prefix'] = '<div class="article-info clearfix">';
       }
       elseif ($variables['node']->field_uib_article_type['und'][0]['value'] == 'event') {
         if (empty($variables['node']->field_uib_kicker['und'][0]['value'])) {
@@ -455,7 +455,7 @@ function uib_zen_preprocess_node(&$variables, $hook) {
           $variables['content']['group_article_top']['#suffix'] = '<div class="service-links">' . $service_links . '</div></div>';
           $variables['content']['group_article_top']['#weight'] = $variables['content']['group_article_main']['#weight'] - 0.5;
           if (empty($variables['content']['group_article_top']['#prefix'])) {
-            $variables['content']['group_article_top']['#prefix'] = '<div class="article-info">';
+            $variables['content']['group_article_top']['#prefix'] = '<div class="article-info clearfix">';
           }
         }
       }
