7bc2ea4ba77be4648269e11228dd69f2292d3747
Are Johannessen <are.johannessen@it.uib.no>
2013-10-03 12:05:33 +0200

    RTS-3827 Add title to full view of testimonial
    The node title is removed in page and added back in preprocess node,
    but only for uib_article. This commit adds it back for content type
    uib_testimonial as well.

--- themes/uib_zen/template.php
+++ themes/uib_zen/template.php
@@ -241,10 +241,14 @@ function uib_zen_preprocess_node(&$variables, $hook) {
    */
   $variables['is_employee'] = FALSE;
 
-  if ($variables['view_mode'] == 'teaser' && $variables['type'] == 'uib_testimonial') {
-    $variables['title'] = '';
-    $variables['content']['field_uib_quote'][0]['#markup'] = '<span class="testimonial-teaser-quote">“</span>' . trim($variables['content']['field_uib_quote'][0]['#markup']) . '<span class="testimonial-teaser-quote">”</span>';
-    $variables['content']['field_uib_name'][0]['#markup'] = '– ' . $variables['content']['field_uib_name'][0]['#markup'];
+  if ($variables['type'] == 'uib_testimonial') {
+    $variables['content']['title']['#markup'] = '<h1>' . $variables['title'] . '</h1>';
+    $variables['content']['title']['#weight'] = -45;
+    if ($variables['view_mode'] == 'teaser') {
+      $variables['title'] = '';
+      $variables['content']['field_uib_quote'][0]['#markup'] = '<span class="testimonial-teaser-quote">“</span>' . trim($variables['content']['field_uib_quote'][0]['#markup']) . '<span class="testimonial-teaser-quote">”</span>';
+      $variables['content']['field_uib_name'][0]['#markup'] = '– ' . $variables['content']['field_uib_name'][0]['#markup'];
+    }
   }
 
   // Add theme suggestion to nodes printed in view mode (newspage).
