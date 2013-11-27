b1ac348c9face43ce29366122cc6c5ff29526053
Gisle Aas <gisle.aas@it.uib.no>
2013-11-27 11:28:05 +0100

    RTS-4537 Cache data replies from FS
    It's cleaner to introduce our own cache table, so we'll do just that.

--- etc/drushrc.php
+++ etc/drushrc.php
@@ -10,6 +10,7 @@ $options['structure-tables'] = array(
        'cache', 'cache_block', 'cache_bootstrap', 'cache_field', 'cache_filter',
        'cache_form', 'cache_image', 'cache_menu', 'cache_page', 'cache_path', 'cache_update',
        'cache_admin_menu', 'cache_coder', 'cache_l10n_update', 'cache_media_xml', 'cache_token',
+       'cache_uib_study',
        'cache_variable', 'cache_views', 'cache_views_data',
        'semaphore', 'sequences', 'queue',
        'history', 'sessions', 'watchdog',
