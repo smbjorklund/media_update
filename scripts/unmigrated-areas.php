4c7cfcc25cd540ca6b25cb277f4bd5c283f40c9e
Gisle Aas <gisle.aas@it.uib.no>
2014-03-21 12:44:44 +0100

    Determine w3 path of migrated areas

--- scripts/unmigrated-areas.php
+++ scripts/unmigrated-areas.php
@@ -6,10 +6,29 @@
 
 $topiclist = simplexml_load_file("http://www.uib.no/topicmap/@@xtopic?type=area&allinstances&limit=0");
 foreach ($topiclist->topic as $topic) {
+  $w2_path = substr($topic['path'], 1);
+
   $query = new EntityFieldQuery;
   $result = $query
     ->entityCondition('entity_type', 'node')
     ->fieldCondition('field_uib_w2_id', 'value', $topic['id'])
     ->execute();
-  print (empty($result) ? "+" : '-') . ' ' . substr($topic['path'], 1) . "\n";
+
+  if (empty($result)) {
+    print "+ $w2_path\n";
+  }
+  else {
+    $nids = array_keys($result['node']);
+    assert(count($nids) == 1, "More than one node with w2_id of " . $topic['id']);
+    $node_path = "node/$nids[0]";
+    foreach (array("nb", "en") as $lang) {
+      $w3_path = drupal_get_path_alias($node_path, $lang);
+      if ($w3_path != $node_path) {
+        $w3_path = "$lang/$w3_path";
+        break;
+      }
+    }
+
+    print "- $w2_path ➡︎ $w3_path\n";
+  }
 }
