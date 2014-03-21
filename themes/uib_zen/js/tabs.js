2445980ea984f442ea03e673d53d7f30db285d39
Are Johannessen <are.johannessen@it.uib.no>
2014-03-21 10:19:14 +0100

    RTS-5609 Added anchor to address line for h2 tabs
    When switching between h2 tabs the url in the address line in the
    browser remains the same. We need to add an anchor to the url when
    switching tabs. Also the anchor should contain the title of the tab.

--- themes/uib_zen/js/tabs.js
+++ themes/uib_zen/js/tabs.js
@@ -1,7 +1,13 @@
 (function ($) {
   Drupal.behaviors.uib_tabs = {
     attach: function (context, settings) {
-        $('.uib-tabs-container').tabs();
+      $('.uib-tabs-container').tabs({
+        show: function(event, ui) {
+          if (ui.panel.id) {
+            window.location.hash = ui.panel.id;
+          }
+        }
+      });
     }
   }
 })(jQuery);
