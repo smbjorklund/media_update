f3579719a6e8f423ed10221b21e6e10bed239a2f
Said Berg <Said.Berg@uib.no>
2014-06-05 08:28:07 +0200

    RTS-6772 Adding label, so the search input is accessible for screen readers

--- themes/uib_zen/templates/page.tpl.php
+++ themes/uib_zen/templates/page.tpl.php
@@ -99,8 +99,10 @@
         <div id="global-searchform">
           <form method="get" action="http://www.uib.no/search">
             <div class="searchbox">
-              <input type="text" id="searchField" name="q" value="" autocomplete="off">
-              <input alt="Submit" type="image" src="<?php print base_path() . drupal_get_path('theme', 'uib_zen'); ?>/images/submit.gif" value="Search">
+              <label style="display:none;" for="searchField"><?php print t('Global search');?></label>
+              <input placeholder="<?php print t('Search UiB') ?>" type="text" id="searchField" name="q" value="" autocomplete="off">
+              <label style="display:none;" for="submitbutton">Search button</label>
+              <input id="submitbutton" alt="Submit" type="image" src="<?php print base_path() . drupal_get_path('theme', 'uib_zen'); ?>/images/submit.gif" value="Search">
             </div>
           </form>
         </div>
