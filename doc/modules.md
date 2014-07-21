c89078d8020f5ab9062027f81a8b5f10206d81b9
Gisle Aas <gisle.aas@it.uib.no>
2014-07-21 13:33:49 +0200

    Visualize the UiB module depdendencies

---
+++ doc/modules.md
@@ -0,0 +1,8 @@
+# Modules
+
+This diagram show the UiB specific modules and their declared dependencies
+on other UiB modules.  There is probably quite a few actual dependencies that
+isn't declared yet. Most modules will also have dependencies on core
+or contributes modules not shown here.
+
+<img src="img/module-dep.svg" style="width: 100%; padding: 2ex;">
---
+++ scripts/uib-mod-dep-in-dot.php
@@ -0,0 +1,28 @@
+<?php
+
+# This script outputs the UiB module dependencies in the GraphViz dot
+# language.  The dot command can be used to convert it to a picture.
+
+print "digraph {\n";
+print "  rankdir=LR;\n";
+print "  node [shape=box, padding=0, fontname=Helvetica];\n";
+
+foreach(system_get_info('module') as $module => $info) {
+  if (substr($module, 0, 3) != 'uib')
+    continue; // skip all non-uib modules
+  if ($module == 'uib')
+    continue; // skip the profile module
+
+  $short_name = $module;
+  if (substr($short_name, 0, 4) == 'uib_')
+    $short_name = substr($short_name, 4);
+  print "  $module [label=\"$short_name\"];\n";
+
+  foreach ($info['dependencies'] as $dep) {
+    if (substr($dep, 0, 3) != 'uib')
+      continue;
+    print "  $module -> $dep;\n";
+  }
+}
+
+print "}\n";
