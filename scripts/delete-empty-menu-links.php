62fc9f5d1a0342fa305569a161f68bfe63741bf8
Gisle Aas <gisle.aas@it.uib.no>
2014-10-14 13:34:16 +0200

    RTS-7209 Run script to remove empty menu links
    I have no idea for how you can create menu links without link text using
    the normal admin pages.  Still we have 6 such links currently. Something
    we inherited from the migration perhaps?
    
    Anyway, they are safe to delete and this avoids poluting out logs with
    "Notice: Trying to get property of non-object in node_page_title()"
    messages.

---
+++ scripts/delete-empty-menu-links.php
@@ -0,0 +1,10 @@
+<?php
+
+$count = 0;
+$result = db_query("SELECT mlid, menu_name, link_path FROM {menu_links} WHERE link_title='' AND} menu_name like 'menu-area-%'");
+foreach ($result as $row) {
+  uibx_log("Removing empty menu link $row->mlid of $row->menu_name referencing $row->link_path");
+  menu_link_delete($row->mlid);
+  $count++;
+}
+uibx_log("$count empty menu links eliminated");
