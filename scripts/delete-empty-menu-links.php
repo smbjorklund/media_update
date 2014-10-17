6c84f872a9ac06b93fa14d6bc9a20399ce304bdd
Gisle Aas <gisle.aas@it.uib.no>
2014-10-17 13:30:08 +0200

    RTS-7209 Removed superfluous }
    Strange thing that it didn't cause synax error from the database
    server.  Drupal must be filtering those out.

--- scripts/delete-empty-menu-links.php
+++ scripts/delete-empty-menu-links.php
@@ -1,7 +1,7 @@
 <?php
 
 $count = 0;
-$result = db_query("SELECT mlid, menu_name, link_path FROM {menu_links} WHERE link_title='' AND} menu_name like 'menu-area-%'");
+$result = db_query("SELECT mlid, menu_name, link_path FROM {menu_links} WHERE link_title='' AND menu_name like 'menu-area-%'");
 foreach ($result as $row) {
   uibx_log("Removing empty menu link $row->mlid of $row->menu_name referencing $row->link_path");
   menu_link_delete($row->mlid);
